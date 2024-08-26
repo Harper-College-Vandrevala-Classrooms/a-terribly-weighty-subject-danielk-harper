31f27a572d0f963f5d281de2be324d208242aa95
danielk-harper <d_kaeseberg1@mail.harpercollege.edu>
2024-08-25 22:21:06 -0500

    Wrote the Converter class to convert ounces to a pounds and ounces to pound ounce format.

--- src/main/java/com/csc/Converter.java
+++ src/main/java/com/csc/Converter.java
@@ -1,12 +1,32 @@
 package com.csc;
 
+import java.util.*;
+
 public class Converter {
 
   public String toPounds(int ounces) {
-    throw new UnsupportedOperationException("Unimplemented method 'toPounds'");
+    // if we have exactly one pound in ounces
+    if (ounces == 16) {
+      return "1.0000 lb";
+    }
+    
+    double pounds = (double)ounces / 16.0;
+    
+    // converts a double to a string with the required precision then returns it.
+    return String.format("%.4f lbs", pounds);
   }
 
   public String toPoundsAndOunces(int ounces) {
-    throw new UnsupportedOperationException("Unimplemented method 'toPoundsAndOunces'");
+    int pounds = ounces / 16;
+    
+    // ounces remaining after dividing out the pounds
+    int ounces_remaining = ounces % 16;
+    
+    // if we need to use lb instead of lbs
+    if (pounds == 1) {
+      return String.format("1 lb %d oz", ounces_remaining);
+    } else {
+      return String.format("%d lbs %d oz", pounds, ounces_remaining);
+    }
   }
 }
